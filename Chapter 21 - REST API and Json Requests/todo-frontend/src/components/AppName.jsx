91d185ce757eff6336921ff8bdffc3a9a8d30f85
Tushar Saha <tusharsahasjd@gmail.com>
2025-07-24 18:06:13 +0530

    Chapter 21 completed

--- Chapter 21 - REST API and Json Requests/todo-frontend/src/components/AppName.jsx
+++ Chapter 21 - REST API and Json Requests/todo-frontend/src/components/AppName.jsx
@@ -1,7 +1,9 @@
-import styles from "./AppName.module.css";
-
 function AppName() {
-  return <h1 className={styles.todoHeading}>TODO App</h1>;
+  return (
+    <h1 className="text-4xl font-bold text-gradient bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-blue-500 py-4">
+      Todo Manager
+    </h1>
+  );
 }
 
 export default AppName;
